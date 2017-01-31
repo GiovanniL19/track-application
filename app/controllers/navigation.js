abcde9632bb027915c8ec17eb0ba9fd68672636d
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-01-31 22:49:07 +0000

    - Implements page indication in navigation

--- app/controllers/navigation.js
+++ app/controllers/navigation.js
@@ -1,6 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  page:{
+    find: true,
+    board: false
+  },
+
   actions:{
+    changePage: function(route){
+      //Resets page type
+      this.set("page", {
+        find: false,
+        board: false
+      });
+
+      //Set the page for page indication in navigation
+      switch(route){
+        case "find":
+          this.set('page.find', true);
+          break;
+
+        case "board":
+          this.set('page.board', true);
+          break;
+      }
+
+      //Transition to route
+      this.transitionToRoute(route);
+
+    }
   }
 });
