d702cbb1dfaa10f170967086a5a3cc3209de9a03
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-02-02 20:46:55 +0000

    - Fixes broken adapter - UI improvements - Spelling correction - Prevents keyboard from showing when selecting date

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
   //Sets up REST adapter to look at localhost server
-  authorizer: 'authorizer:token,
+  authorizer: 'authorizer:token',
 	host: 'http://localhost:3002'
 });
