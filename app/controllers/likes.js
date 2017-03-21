d4cd4f1821835a696b481f4ddd3ce48f44c03db3
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-03-21 19:37:01 +0000

    #29 - Implements unlike journey functionality

--- app/controllers/likes.js
+++ app/controllers/likes.js
@@ -2,9 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   navigation: Ember.inject.service(),
+  alert: Ember.inject.service("alert-message"),
   application: Ember.inject.controller(),
 
   actions: {
+    removeLiked(journey){
+      let controller = this;
+      let userID = journey.get("starred.id");
+
+      this.store.find("user", userID).then(function (user) {
+        user.get("starredJourneys").removeObject(journey);
+
+        user.save().then(function(){
+          journey.destroyRecord().then(function(){
+            controller.set("alert.message", "Journey Removed");
+          });
+        });
+
+      });
+    },
     select(journey){
       this.transitionToRoute("find-results", {queryParams: {
         origin: journey.get("from.name"),
