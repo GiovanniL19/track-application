c1e1edf4481f95efd6748e327d4e6f9c840ab364
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-04-05 14:48:57 +0100

    - Fixes bugs #56 & #55

--- app/components/train-card.js
+++ app/components/train-card.js
@@ -5,11 +5,13 @@ export default Ember.Component.extend({
   train: Ember.inject.service(),
   details: null,
   destination: "",
+  destinationCRS: "",
 
   actions:{
     select(){
       this.get("train").setDetails(this.get("details"));
       this.set("train.userDestination", this.get("destination"));
+      this.set("train.userDestinationCRS", this.get("destinationCRS"));
       this.get("routing").transitionTo("train-information");
     }
   }
--- app/services/train.js
+++ app/services/train.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   details: null,
   userDestination: "",
+  userDestinationCRS: "",
   setDetails(details) {
     this.set("details", details);
   }
